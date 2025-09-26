436767467eb0843e3b6618f6e7e1dc483347f98b
Tahmin-Muntaha <tahminmuntaha66@gmail.com>
2025-09-26 22:30:10 +0600

    conditional css

--- src/components/counties/countries.jsx
+++ src/components/counties/countries.jsx
@@ -1,17 +1,20 @@
 import React, { use } from 'react';
 import Country from './country/country';
-
+import './countries.css'
 const Countries = ({countriesPromise}) => {
     const countriesData=use(countriesPromise)
     const countries=countriesData.countries
     console.log(countries)
     
     return (
-        <div>
-           <h1>In the Countries : {countries.length}</h1>
+        <div class='h1'>
+            <h1>In the Countries : {countries.length}</h1>
+        <div className='countries'>
+           
            {
                 countries.map(country=><Country key={country.cca3.cca3} country={country}></Country>)
            }
+        </div>
         </div>
     );
 };
--- src/components/counties/country/country.jsx
+++ src/components/counties/country/country.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './country.css'
 const Country = ({country}) => {
-    
+    const [visited,setVisited]=useState(false)
+    const handle=()=>{
+
+        if(visited){
+            setVisited(false)
+        }
+        else{
+            setVisited(true)
+        }
+    }
     return (
-        <div>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt}></img>
             <h3>Name : {country.name.common} </h3>
             <p>Population : {country.population.population}</p>
+            <p>Area : {country.area.area}
+                       {country.area.area > 30000?" Big Country ":" Small Country "}
+            </p>
+            <button onClick={handle}>
+                {visited?" Visited ":" Not Visited "}
+            </button>
         </div>
     );
 };
